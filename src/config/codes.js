58fd83fafd7c54ea10c62c738f2a9276116ec4d8
Atlas Tawjih <fouzanlahcen@gmail.com>
2026-05-25 20:36:12 +0200

    ENNACHI#2026

--- src/config/codes.js
+++ src/config/codes.js
@@ -30,7 +30,8 @@ export const CODES_VALIDES = [
   'CHARYF#2026',
   'KAOUTAR#2026',
   'AITTMGUROUT#2026',
-  'NADIR#2026'
+  'NADIR#2026',
+  'ENNACHI#2026'
   // Ajoute tes codes ici, un par ligne
 ]
 
