24dd5882cd287c108783659914c99e9a10ea0b84
Atlas Tawjih <fouzanlahcen@gmail.com>
2026-05-28 18:32:28 +0200

    LAAMECH#2026

--- src/config/codes.js
+++ src/config/codes.js
@@ -55,7 +55,8 @@ export const CODES_VALIDES = [
   'ELMESSYEH#26',
   'ELBAZE#2026',
   'ELATTAOUI#2026',
-  'SABOUNI#2026'
+  'SABOUNI#2026',
+  'LAAMECH#2026'
   // Ajoute tes codes ici, un par ligne
 ]
 
